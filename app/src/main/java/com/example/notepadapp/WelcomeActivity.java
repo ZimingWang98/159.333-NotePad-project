dc04ce497b47e1b27ed98272b2ec64bcf23cbe6c
ZimingWang98 <1019969724@qq.com>
2020-03-10 14:35:00 +0800

    NotePadApp

---
+++ app/src/main/java/com/example/notepadapp/WelcomeActivity.java
@@ -0,0 +1,142 @@
+package com.example.notepadapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private ViewPager screenPager;
+    WelcomeViewPageAdapter welcomeViewPageAdapter;
+    TabLayout tab;
+    Button btnNext;
+    int position=0;
+    Button btnStarted;
+    Animation btnAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //make activity on full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //check the app is the first open or not
+        if(restorePrefData()){
+            Intent loginActivity=new Intent(getApplicationContext(),LoginActivity.class);
+            startActivity(loginActivity);
+        }
+        setContentView(R.layout.activity_welcome);
+        //hide action bar
+        getSupportActionBar().hide();
+
+        //initial view
+        tab=findViewById(R.id.tab_layout);
+        btnNext=findViewById(R.id.btn_next);
+        btnStarted=findViewById(R.id.btn_started);
+        btnAnim= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.btn_animation);
+
+
+
+        final List<ScreenItem> mList=new ArrayList<>();
+        mList.add(new ScreenItem("Step   one","Register your own account",R.drawable.register));
+        mList.add(new ScreenItem("Step   Two","Log into your account",R.drawable.login));
+        mList.add(new ScreenItem("Step Three","Enjoy your personal secret note",R.drawable.enjoy));
+
+        //set viewpager
+        screenPager=findViewById(R.id.screen_pager);
+        welcomeViewPageAdapter=new WelcomeViewPageAdapter(this,mList);
+        screenPager.setAdapter(welcomeViewPageAdapter);
+
+        tab.setupWithViewPager(screenPager);
+        //next button click listener
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                position= screenPager.getCurrentItem();
+                if(position<mList.size()){
+
+                    position++;
+                    screenPager.setCurrentItem(position);
+
+                }
+                if(position==mList.size()-1){
+                    loadLastScreen();
+                }
+
+
+            }
+        });
+        //tab add change listener
+        tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getPosition()==mList.size()-1){
+                    loadLastScreen();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        //Started button click listener
+        btnStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open login activity
+                Intent loginActivity=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(loginActivity);
+                //use shared preferences to make sure welcome page already been checked
+                savePrefsData();
+                finish();
+            }
+        });
+    }
+
+    private boolean restorePrefData(){
+        SharedPreferences pref=getApplicationContext().getSharedPreferences("mPrefs",MODE_PRIVATE);
+        Boolean isWelcomeActivityOpenedBefore=pref.getBoolean("isWelcomeOpened",false);
+
+        return isWelcomeActivityOpenedBefore;
+    }
+    private void savePrefsData() {
+
+        SharedPreferences pref=getApplicationContext().getSharedPreferences("mPrefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor=pref.edit();
+        editor.putBoolean("isWelcomeOpened",true);
+        editor.commit();
+
+    }
+
+    //show the Started button and hide the tab and next button
+    private void loadLastScreen() {
+
+        btnStarted.setVisibility(View.VISIBLE);
+        btnNext.setVisibility(View.INVISIBLE);
+        tab.setVisibility(View.INVISIBLE);
+        //setup animation
+        btnStarted.setAnimation(btnAnim);
+
+    }
+}
---
+++ app/src/main/java/com/example/notepadapp/WelcomeViewPageAdapter.java
@@ -0,0 +1,54 @@
+package com.example.notepadapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class WelcomeViewPageAdapter extends PagerAdapter {
+    Context mContext;
+    List<ScreenItem> mListScreen;
+
+    public WelcomeViewPageAdapter(Context mContext, List<ScreenItem> mListScreen) {
+        this.mContext = mContext;
+        this.mListScreen = mListScreen;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layoutScreen = inflater.inflate(R.layout.layout_screen,null);
+
+        ImageView imgSlide=layoutScreen.findViewById(R.id.welcome_image);
+        TextView title=layoutScreen.findViewById(R.id.welcome_title);
+        TextView description=layoutScreen.findViewById(R.id.welcome_description);
+        title.setText(mListScreen.get(position).getTitle());
+        description.setText(mListScreen.get(position).getDescription());
+        imgSlide.setImageResource(mListScreen.get(position).getScreenImage());
+        container.addView(layoutScreen);
+        return layoutScreen;
+    }
+
+    @Override
+    public int getCount() {
+        return mListScreen.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object obj) {
+        return view==obj;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object obj) {
+        container.removeView((View) obj);
+    }
+}
