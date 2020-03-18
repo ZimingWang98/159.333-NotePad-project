9082b5171512e91e1ee3efcb12c540f666120cda
ZimingWang98 <61429778+zimingwang98@users.noreply.github.com>
2020-03-18 17:21:12 +0800

    Add the Database for user information

--- app/src/main/java/com/example/notepadapp/LoginActivity.java
+++ app/src/main/java/com/example/notepadapp/LoginActivity.java
@@ -1,15 +1,21 @@
 package com.example.notepadapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //getActionBar().hide();
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
