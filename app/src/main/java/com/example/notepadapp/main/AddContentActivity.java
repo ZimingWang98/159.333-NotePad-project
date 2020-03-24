5c217dd86281fdd196571b6ca611e0d5a0407d54
ZimingWang98 <61429778+zimingwang98@users.noreply.github.com>
2020-03-24 15:16:59 +0800

    Add Main Activity files for user to add note

--- app/src/main/java/com/example/notepadapp/main/AddContentActivity.java
+++ app/src/main/java/com/example/notepadapp/main/AddContentActivity.java
@@ -2,10 +2,12 @@ package com.example.notepadapp.main;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.Button;
 
 public class AddContentActivity extends Activity implements View.OnClickListener {
 
     String content;
+    Button add_save,add_cancel;
     @Override
     public void onClick(View view) {
 
