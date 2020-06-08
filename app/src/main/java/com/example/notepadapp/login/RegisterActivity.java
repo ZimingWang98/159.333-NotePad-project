e1bf20de4223cd93d2150b71401aff17cb8b9157
ZimingWang98 <61429778+zimingwang98@users.noreply.github.com>
2020-06-08 21:27:30 +0800

    Adding MD5 to encrypt the password

--- app/src/main/java/com/example/notepadapp/login/RegisterActivity.java
+++ app/src/main/java/com/example/notepadapp/login/RegisterActivity.java
@@ -62,11 +62,9 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
         String s = null;
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
-            //这两行代码的作用是：
-            // 将bytes数组转换为BigInterger类型。1，表示 +，即正数。
+            //change the bytes array to BigInteger
             BigInteger bigInt = new BigInteger(1, md.digest(sourceStr.getBytes()));
-            // 通过format方法，获取32位的十六进制的字符串。032,代表高位补0 32位，X代表十六进制的整形数据。
-            //为什么是32位？因为MD5算法返回的时一个128bit的整数，我们习惯于用16进制来表示，那就是32位。
+            //use format to get 32-bit hexadecimal string
             s = String.format("%032x", bigInt);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
